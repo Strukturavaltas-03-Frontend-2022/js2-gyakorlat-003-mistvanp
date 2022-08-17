fb5c3a2f3e1dc08503e9a38ec6ac5232e14a7783
mistvanp <marai.istvanp@gmail.com>
2022-08-17 18:34:06 +0200

    Done

--- solutions/app1.js
+++ solutions/app1.js
@@ -3,6 +3,12 @@ const user = {
   lastName: 'Doe',
 };
 
+const {
+  firstName,
+  lastName,
+  jobb = 'unknown'
+} = user;
+
 //
 
 // export {
@@ -10,3 +16,17 @@ const user = {
 //   lastName,
 //   job,
 // };
+
+
+// ## 1. feladat
+// - A `solutions/app1.js` fájlban dolgozz!
+// - Adott az alábbi objektum:
+// ```javascript
+//     const user = {
+//         firstName: 'John',
+//         lastName: 'Doe'
+//     };
+// ```
+// - Mentsd el külön lastName, firstName és job változókba a fenti objektum 
+// property-jeit destructuring használatával!
+// - Ha valamelyik nincs megadva, akkor az érték legyen az "unknown" string!
